1f60147e290cebf72f72ff943356dce8429b8ce0
Nikita <nik.lapin1107@gmail.com>
2023-12-16 12:24:03 +0500

    Add middleware

--- forum_profile/admin.py
+++ forum_profile/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from forum_profile.models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    exclude = ['id']
+    search_fields = ['username',
+                     'description',
+                     'signature',
+                     'profile_color',]
+
+    list_display = ['username',
+                    'photo',
+                    'profile_color',
+                    'user']
+
+    list_editable = ['photo',
+                     'profile_color']
